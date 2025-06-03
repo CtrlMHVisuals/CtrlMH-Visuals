906df65a69463850ced9e0a2bd80d7924a6de47d
CtrlMHVisuals <MaliqueTemp@gmail.com>
2025-06-03 18:14:00 -0400

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./globals.css";
-export const metadata = { title: "Ctrl+MH Visuals", description: "Portfolio site" };
-export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
-}
