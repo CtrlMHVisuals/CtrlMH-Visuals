b862667e3d07c6b0398abd274d2006ca8e7b351b
CtrlMHVisuals <MaliqueTemp@gmail.com>
2025-06-03 18:14:11 -0400

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">Ctrl+MH Visuals</h1>
-    </main>
-  );
-}
