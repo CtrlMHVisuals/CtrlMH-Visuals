b5f0ef69d67b67a075b9350d0c6d2b9af835776d
CtrlMHVisuals <MaliqueTemp@gmail.com>
2025-06-03 14:08:53 -0400

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white text-black">
-      <h1 className="text-4xl font-bold mb-4">Ctrl+MH Visuals</h1>
-      <p className="text-lg text-gray-600">Welcome to the portfolio.</p>
-    </main>
-  )
-}
