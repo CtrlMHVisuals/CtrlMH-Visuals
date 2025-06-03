07ceaf1e8c6e7edea8c2ec604857568bc16edc53
CtrlMHVisuals <MaliqueTemp@gmail.com>
2025-06-03 14:20:06 -0400

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Ctrl+MH Visuals</h1>
+    </main>
+  );
+}
